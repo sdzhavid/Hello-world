c475e06abf4ce825aed534669428c9241b1ea4da
Samir Dzhavid <56024003+sdzhavid@users.noreply.github.com>
2020-09-27 20:16:37 +0100

    Demo

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * A demo class
+ *
+ * @author Samir Dzhavid
+ * @version September 2020
+ */
+public class HelloWorld {
+    /**
+     * A demo method
+     * @param args command line arguments ( not used )
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
---
+++ tests/HelloWorldTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+    @Test
+    public void passTest(){
+    }
+
+    @Test
+    public void failTest(){
+        fail();
+    }
+
+}
